0583ef2478d5eda3f7d5aa568328041d71fac4cd
bhushan354 <deshmukhumesh354@gmail.com>
2023-08-07 15:44:11 +0530

    add Book function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const bookList = document.getElementById('book-list');
 
+
 const books = [
   {
     title: 'Lorem Ipsum',
@@ -13,20 +14,39 @@ const books = [
 
 function display() {
   let displayBook = '';
-  for (let i = 0; i < 6; i += 1) {
+  for (let i = 0; i < books.length; i += 1) {
     displayBook += `
 
                 <div class="card">
-                    <ul>
-                        <li>${books[i].title}</li>
-                        <li>${books[i].author}</li>
-                    </ul>
+                  
+                        <p>${books[i].title}</p>
+                        <p>${books[i].author}</p>
+                
                     <button type="submit" id="remove-btn">Remove</button>
+
+                    <hr/>
                 </div>
     
        `;
   }
   bookList.innerHTML = displayBook;
 }
+
+
+
+//add book section
+const getTitle = localStorage.getItem('title');
+const getAuthor = localStorage.getItem('author');
+
+
+function addBook() {
+  const newBook = { title : title.value = getTitle, author : author.value = getAuthor};
+  books.push(newBook);
+
+}
+const addBtn = document.querySelector('.btn');
+
+addBtn.addEventListener('click',addBook
+());
 
 display();
