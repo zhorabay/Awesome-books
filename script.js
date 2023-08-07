7056a205fabf5b4652cb574474511b455a6c09c3
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-08-07 11:41:30 +0300

    Use an array of objects

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ const bookList = document.getElementById('book-list');
 const books = [
   {
     title: 'Lorem Ipsum',
-    author: 'Testeroo Testyy'
+    author: 'Testeroo Testyy',
   },
   {
     title: 'Second Book',
-    author: 'Testeroo Testyy'
+    author: 'Testeroo Testyy',
   },
 ];
 
 function display() {
-    let displayBook = '';
-    for (let i = 0; i < 6; i ++) {
-      displayBook += `
+  let displayBook = '';
+  for (let i = 0; i < 6; i += 1) {
+    displayBook += `
 
                 <div class="card">
                     <ul>
-                        <li>${booksData[i].title}</li>
-                        <li>${booksData[i].author}</li>
+                        <li>${books[i].title}</li>
+                        <li>${books[i].author}</li>
                     </ul>
                     <button type="submit" id="remove-btn">Remove</button>
                 </div>
     
        `;
-    }
-    bookList.innerHTML = displayBook;
   }
-  
-  display();
+  bookList.innerHTML = displayBook;
+}
+
+display();
